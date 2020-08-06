e9a12cc1964c8f1ae2364c2e727f25a1e307958a
Hguedez <57779032+Hguedez@users.noreply.github.com>
2020-08-06 00:01:25 -0400

    register added

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  validateConfig: FormGroup;
+  constructor(private _builder:FormBuilder,private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  configValidation(){
+    let formData = new FormData();
+    formData.append('Ubicacion', this.validateConfig.get('Ubicacion').value);
+    
   }
 
 }
